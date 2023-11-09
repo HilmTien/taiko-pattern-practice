398a978f9f3decb1e963a4a82004e43306f5f7bb
Tien <tient2477@gmail.com>
2023-11-09 12:04:01 +0100

    :sparkles: Add pattern component

--- src/components/pattern.tsx
+++ src/components/pattern.tsx
@@ -1,3 +1,4 @@
+import { HITCIRCLE_SIZE } from "@/components/hitcircle";
 import { Don, DonFinisher, Kat, KatFinisher } from "@/components/hitcircles";
 
 type PatternProps = {
@@ -6,25 +7,65 @@ type PatternProps = {
 
 export default function Pattern({ patternString }: PatternProps) {
   let current_x = 0;
+  let sv = 0.75
+  let delta_x = HITCIRCLE_SIZE.normal * sv
+
   const hitobjects = [];
 
-  //   for (let i = patternString.length; i > 0; i--) {
-  //     switch (patternString[i]) {
-  //       case "d": {
-  //         hitobjects.push([<Don position={current_x} />]);
-  //         break;
-  //       }
-  //       case "k": {
-  //       }
-  //     }
-  //   }
+  for (let i = patternString.length; i > 0; i--) {
+    switch (patternString[i - 1]) {
+      // Snapping changes
+      case "]": {
+        delta_x = HITCIRCLE_SIZE.normal * sv / 2;
+        break;
+      }
+      case ")": {
+        delta_x = HITCIRCLE_SIZE.normal * sv / 1.5;
+        break;
+      }
+      case "(": {
+        current_x += HITCIRCLE_SIZE.normal * sv / 3;
+        delta_x = HITCIRCLE_SIZE.normal * sv;
+        break;
+      }
+      case "[": {
+        current_x += HITCIRCLE_SIZE.normal * sv / 2;
+        delta_x = HITCIRCLE_SIZE.normal * sv;
+        break;
+      }
+
+      // Blank note
+      case "-": case "x": case " ": {
+        current_x += delta_x
+        break
+      }
+
+      // Regular notes
+      case "d": {
+        hitobjects.push([<Don position={current_x} />]);
+        current_x += delta_x;
+        break;
+      }
+      case "D": {
+        hitobjects.push([<DonFinisher position={current_x} />]);
+        current_x += delta_x;
+        break;
+      }
+      case "k": {
+        hitobjects.push([<Kat position={current_x} />]);
+        current_x += delta_x;
+        break;
+      }
+      case "K": {
+        hitobjects.push([<KatFinisher position={current_x} />]);
+        current_x += delta_x;
+        break;
+      }
+    }
+  }
   return (
     <>
-      <Don position={0}></Don>
-      <Don position={96}></Don>
-      <Kat position={96 * 2}></Kat>
-      <Kat position={96 * 3}></Kat>
-      <Don position={96 * 3 + 96 / 1.5}></Don>
+      {hitobjects.map((hitobject) => hitobject)}
     </>
   );
 }
